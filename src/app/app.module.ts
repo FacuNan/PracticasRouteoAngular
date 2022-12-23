d33e10dac0b4e7d97cd01dab90e15449924530e4
Facundo Comercio <facundocomercio@outlook.com>
2022-12-23 15:53:22 -0300

    agregando login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/page/home/home.component';
@@ -8,6 +8,7 @@ import { LoginComponent } from './components/page/login/login.component';
 import { NotFoundComponent } from './components/page/not-found/not-found.component';
 import { ContactosComponent } from './components/page/contactos/contactos.component';
 import { DetallesContactoComponent } from './components/page/detalles-contacto/detalles-contacto.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,10 @@ import { DetallesContactoComponent } from './components/page/detalles-contacto/d
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
